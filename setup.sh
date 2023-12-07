47f62c3405210e7aba703d441aee461271a56fc3
Brochot Mathieu <58740406+mBrochot@users.noreply.github.com>
2023-12-07 22:31:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -75,47 +75,4 @@ echo "Cleaning up brew 🧹"
 brew cleanup
 
 
-# ------- VS Code -------
-echo "Installing VS Code extensions... 👨‍💻"
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension github.copilot
-code --install-extension github.copilot-chat
-code --install-extension formulahendry.auto-close-tag
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension steoates.autoimport
-code --install-extension naumovs.color-highlight
-code --install-extension wallabyjs.console-ninja
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-ceintl.vscode-language-pack-fr
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode.vscode-typescript-next
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension carbonid1.emmet-jss
-code --install-extension usernamehw.errorlens
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension eamodio.gitlens
-code --install-extension hashicorp.terraform
-code --install-extension heybourn.headwind
-code --install-extension sburg.vscode-javascript-booster
-code --install-extension ritwickdey.liveserver
-code --install-extension bierner.markdown-preview-github-styles
-code --install-extension pkief.material-icon-theme
-code --install-extension moserjose.mosmmy-icons-vscode
-code --install-extension cweijan.vscode-mysql-client2
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension christian-kohler.path-intellisense
-code --install-extension esbenp.prettier-vscode
-code --install-extension urbantrout.refactor-css
-code --install-extension humao.rest-client
-code --install-extension hex-ci.stylelint-plus
-code --install-extension bradlc.vscode-tailwindcss
-code --install-extension meganrogge.template-string-converter
-code --install-extension rangav.vscode-thunder-client
-code --install-extension planbcoding.vscode-react-refactor
-code --install-extension shyykoserhiy.vscode-spotify
-code --install-extension mikestead.dotenv
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension tombonnike.vscode-status-bar-format-toggle
-
-
 echo "Done! 🎉"
